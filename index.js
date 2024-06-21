5a77fd81fe3c146cb02e68380f77f447c59f9ca1
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-06-21 21:54:46 +0200

    check 4.0

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let myWork = [];
+let car = {
+	model: "Golf",
+	make: "Volkswagen",
+	year: 1999,
+	color: "black",
+};
 
-for (let i = 0; i <= 10; i++) {
-	let obj = {
-		lesson: i,
-		stat: i % 2 == 0 ? true : false,
-	};
-	myWork.push(obj);
+for (let key in car) {
+	console.table(car[key]);
+	console.table(key);
 }
-console.log(myWork);
