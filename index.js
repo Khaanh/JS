5dd42fe8a6ff67f2e84d5a1d84f66b189277a84d
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-06-22 16:08:54 +0200

    check 5.0

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-let car = {
-	model: "Golf",
-	make: "Volkswagen",
-	year: 1999,
-	color: "black",
-};
+let cars = [
+	{
+		model: "Golf",
+		make: "Volkswagen",
+		year: 1999,
+		color: "black",
+	},
+	{
+		model: "Picanto",
+		make: "Kia",
+		year: 2020,
+		color: "red",
+	},
+	{
+		model: "Peugeot",
+		make: "208",
+		year: 2021,
+		color: "black",
+	},
+	{
+		model: "Fiat",
+		make: "Punto",
+		year: 2020,
+		color: "black",
+	},
+];
 
-for (let key in car) {
-	console.table(car[key]);
-	console.table(key);
+for (let i = 0; i < 10; i++) {
+	console.log(i);
+	if (i == 5) {
+		break;
+	}
 }
