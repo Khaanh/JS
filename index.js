75931c30b6b1a02a56a80516917bf7747c0f75ce
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-06-23 13:29:30 +0200

    check 6.0

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
-let cars = [
-	{
-		model: "Golf",
-		make: "Volkswagen",
-		year: 1999,
-		color: "black",
-	},
-	{
-		model: "Picanto",
-		make: "Kia",
-		year: 2020,
-		color: "red",
-	},
-	{
-		model: "Peugeot",
-		make: "208",
-		year: 2021,
-		color: "black",
-	},
-	{
-		model: "Fiat",
-		make: "Punto",
-		year: 2020,
-		color: "black",
-	},
-];
-
-for (let i = 0; i < 10; i++) {
-	console.log(i);
-	if (i == 5) {
-		break;
-	}
+function someFunc(x, y) {
+	console.log(x, y);
 }
+let arr = [1, 2, 3, 4, 5, 6];
+someFunc("x", [...arr]);
