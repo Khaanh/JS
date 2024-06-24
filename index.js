3bb61072b6b00ae05f2ee0185a134f17be905d64
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-06-24 22:26:52 +0200

    Check 7.0

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
-function someFunc(x, y) {
-	console.log(x, y);
+class Employee {
+	#firstName;
+	#lastName;
+	#yearsOfWork;
+
+	constructor(firstName, lastName, yearsOfWork) {
+		this.#firstName = firstName;
+		this.#lastName = lastName;
+		this.#yearsOfWork = yearsOfWork;
+	}
+
+	get firstName() {
+		return this.#firstName;
+	}
+
+	set firstName(firstName) {
+		this.#firstName = firstName;
+	}
+
+	get lastName() {
+		return this.#lastName;
+	}
+
+	set lastName(lastName) {
+		this.#lastName = lastName;
+	}
+
+	get yearsOfWork() {
+		return this.#yearsOfWork;
+	}
+
+	set yearsOfWork(years) {
+		this.#yearsOfWork = years;
+	}
 }
-let arr = [1, 2, 3, 4, 5, 6];
-someFunc("x", [...arr]);
+
+let alex = new Employee("Alex", "Hugo", 20);
+let bob = new Employee("Bob", "Marlin", 4);
+
+let arr = [alex, bob];
+console.log(arr);
+
+Employee.prototype.getInfo = function () {
+	console.log(
+		`${this.firstName}${this.lastName} and working here: ${this.yearsOfWork}`
+	);
+};
+
+alex.getInfo();
