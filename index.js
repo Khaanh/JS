997278a7d2d66d379881a84ac6efc714d3630c15
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-06-25 23:11:09 +0200

    Check 8.0

--- index.js
+++ index.js
@@ -1,49 +1,16 @@
-class Employee {
-	#firstName;
-	#lastName;
-	#yearsOfWork;
+let arr = [
+	"Laurence",
+	"Mike",
+	"Larry",
+	"Kim",
+	"Joanne",
+	"Laurence",
+	"Mike",
+	"Laurence",
+	"Mike",
+	"Laurence",
+	"Mike",
+];
 
-	constructor(firstName, lastName, yearsOfWork) {
-		this.#firstName = firstName;
-		this.#lastName = lastName;
-		this.#yearsOfWork = yearsOfWork;
-	}
-
-	get firstName() {
-		return this.#firstName;
-	}
-
-	set firstName(firstName) {
-		this.#firstName = firstName;
-	}
-
-	get lastName() {
-		return this.#lastName;
-	}
-
-	set lastName(lastName) {
-		this.#lastName = lastName;
-	}
-
-	get yearsOfWork() {
-		return this.#yearsOfWork;
-	}
-
-	set yearsOfWork(years) {
-		this.#yearsOfWork = years;
-	}
-}
-
-let alex = new Employee("Alex", "Hugo", 20);
-let bob = new Employee("Bob", "Marlin", 4);
-
-let arr = [alex, bob];
-console.log(arr);
-
-Employee.prototype.getInfo = function () {
-	console.log(
-		`${this.firstName}${this.lastName} and working here: ${this.yearsOfWork}`
-	);
-};
-
-alex.getInfo();
+let newArr = arr.filter((el) => console.log(arr.indexOf(el)));
+console.log(newArr);
