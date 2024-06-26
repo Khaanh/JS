35c29661eda12393cf09b02234014e1f5ca0da3c
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-06-26 22:00:05 +0200

    Check 9.0

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-let arr = [
-	"Laurence",
-	"Mike",
-	"Larry",
-	"Kim",
-	"Joanne",
-	"Laurence",
-	"Mike",
-	"Laurence",
-	"Mike",
-	"Laurence",
-	"Mike",
-];
+let url = "https://jsonplaceholder.typicode.com/users";
 
-let newArr = arr.filter((el) => console.log(arr.indexOf(el)));
-console.log(newArr);
+async function getComments(url) {
+	let resp = await fetch(url);
+	let comment = await resp.json();
+}
+
+getComments(url);
