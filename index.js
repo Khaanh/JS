36ec04749ac1eebf8c6a26a3e68e0c0e9d6733e5
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-06-27 22:17:07 +0200

    Check 10.0

--- index.js
+++
@@ -1,8 +0,0 @@
-let url = "https://jsonplaceholder.typicode.com/users";
-
-async function getComments(url) {
-	let resp = await fetch(url);
-	let comment = await resp.json();
-}
-
-getComments(url);
